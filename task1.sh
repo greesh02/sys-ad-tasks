73af1d3b5e3c0dd0f86015d8d85b033babdea72c
greesh02 <61308345+greesh02@users.noreply.github.com>
2020-04-26 13:23:32 +0530

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -2,9 +2,6 @@
 
 n=1
 
-sudo groupadd Army
-sudo groupadd Navy
-sudo groupadd Airforce
 
 sudo useradd ChiefCommander  -m -s /bin/bash
 sudo useradd ArmyGeneral  -m -s /bin/bash
@@ -15,7 +12,7 @@ while [ $n -le 50 ]
 do 
    echo $n
    sudo useradd Army$n -m -s /bin/bash
-   sudo adduser Army$n Army
+   
    n=$(( n+1 ))
 done
 
@@ -25,7 +22,7 @@ while [ $n -le 50 ]
 do 
    echo $n
    sudo useradd Navy$n  -m -s /bin/bash
-   sudo adduser Navy$n Navy
+   
    n=$(( n+1 ))
 done
 
@@ -35,7 +32,7 @@ while [ $n -le 50 ]
 do 
    echo $n
    sudo useradd Airforce$n  -m -s /bin/bash
-   sudo adduser Airforce$n Airforce
+   
    n=$(( n+1 ))
 done
 
